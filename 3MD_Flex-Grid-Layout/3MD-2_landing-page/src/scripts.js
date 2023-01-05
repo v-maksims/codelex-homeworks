b68c2b8a2bbb843ddeb10bb10005ed3000643722
Maksims Veiserts <maksims.veiserts@gmail.com>
2023-01-05 11:05:54 +0200

    edits

---
+++ 3MD_Flex-Grid-Layout/3MD-2_landing-page/src/scripts.js
@@ -0,0 +1,5 @@
+console.log('ready');
+
+document.querySelector('.header__burger-btn').onclick = function () {
+    alert('tap');
+}
--- 3MD_Flex-Grid-Layout/3MD-2_landing-page/src/scripts.ts
+++
@@ -1,9 +0,0 @@
-console.log('ready');
-$(function () {
-    $('.header__burger-btn').on('click', function () {
-        $('.header__navigation-menu').removeClass('navigation-menu--close')
-    });
-    $('.header__burger-btn').on('click', function () {
-        $('.header__navigation-menu').addClass('navigation-menu--close')
-    });
-})
