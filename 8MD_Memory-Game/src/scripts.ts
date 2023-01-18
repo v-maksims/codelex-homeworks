b3d31901a257f309609c06d0e77846586fca047d
Maksims Veiserts <maksims.veiserts@gmail.com>
2023-01-18 20:07:02 +0200

    8MD game some works

--- 8MD_Memory-Game/src/scripts.ts
+++ 8MD_Memory-Game/src/scripts.ts
@@ -1,145 +1,180 @@
 import _, { indexOf } from 'lodash';
-import lodash from 'lodash'
+import lodash from 'lodash';
 import { cli } from 'webpack';
+import swal from 'sweetalert';
 
 // Buttons
+const btnStart = document.querySelector('.btn__start-game');
+const btnReset = document.querySelector('.btn__reset-game');
+const btnGameModes = document.querySelectorAll('.js-game-field');
+const btnStartReset = document.querySelector('.btn-game');
+const btnModeList = document.querySelector('.btn-mode');
+const moveCounter = document.querySelector('.js-move-counter');
+const winCounter = document.querySelector('.js-win-counter');
 
-const btnStart = document.querySelector('.btn__start-game'); // Start button
-const btnRestart = document.querySelector('.btn__reset-game'); // Restart button
+// Card list for creating cards
+const gameField = document.querySelector('.card__list');
+// eslint-disable-next-line no-undef
+let cardList: NodeListOf<HTMLElement> = document.querySelectorAll('.card');
 
-// Cards list without class 'card-id'
-const cardsList = document.querySelectorAll<HTMLElement>('.card__item')
+// Variables
+const cardColors = ['#D8AE77', '#6B633E', '#CBC195', '#342A69', '#EF2E72', '#005699', 'rgb(32, 32, 32)'];
+const [color1, color2, color3, color4, color5, color6, colorBG] = cardColors;
+const cardIDs: number[] = [];
 
-// Cards color with [0-1, 2-3, 4-5, default]
-const cardColors = ['#D8AE77', '#6B633E', '#CBC195', 'rgb(32, 32, 32)']
+let gameFieldCount: number;
+let startBtnClick = 0;
+let randomCardIDs: number[];
+let previousCard: HTMLElement | null = null;
+let previousCards: string[] = [];
+let moveCount = 0;
+let pairCount = 0;
+let winTimes = 0;
 
-// Game variables
-
-let moveCount = 0; // Move count
-let previousCard: HTMLElement | null = null; // The previous card to compare with the second selected one
-let previous: string[] = []; // Array with two cards for background comparison
-let clickTime = 0; // Click times on button start it is only 0 or 1
-let cardPair = 0; // Counter for counting pairs (for an even number of cards)
-
-
+// Loop for gameField
+for (let i = 0; i < btnGameModes.length; i += 1) {
+  // eslint-disable-next-line no-loop-func
+  btnGameModes[i].addEventListener('click', () => {
+    btnModeList.classList.add('btn-mode--disabled');
+    btnStartReset.classList.remove('btn-game--disabled');
+    if (btnGameModes[i].classList[1] === 'btn__mode-3x2') {
+      gameFieldCount = 6;
+      for (let j = 0; j < gameFieldCount; j += 1) {
+        cardIDs.push(j);
+      }
+      randomCardIDs = _.shuffle(cardIDs);
+    } else {
+      gameFieldCount = 12;
+      for (let j = 0; j < gameFieldCount; j += 1) {
+        cardIDs.push(j);
+      }
+      randomCardIDs = _.shuffle(cardIDs);
+    }
+  });
+}
 
 // Functions
+const handleCardClick = (index: number) => {
+  const selected = cardList[index].classList[1];
 
-// Start game, the number of cards is counted athen the numbers of the cards are mixed (id). And at the end, a class is added to each card with its own indicator (card-id)
+  if (selected === 'card-0' || selected === 'card-1') {
+    cardList[index].style.backgroundColor = color1;
+  } else if (selected === 'card-2' || selected === 'card-3') {
+    cardList[index].style.backgroundColor = color2;
+  } else if (selected === 'card-4' || selected === 'card-5') {
+    cardList[index].style.backgroundColor = color3;
+  } else if (selected === 'card-6' || selected === 'card-7') {
+    cardList[index].style.backgroundColor = color4;
+  } else if (selected === 'card-8' || selected === 'card-9') {
+    cardList[index].style.backgroundColor = color5;
+  } else if (selected === 'card-10' || selected === 'card-11') {
+    cardList[index].style.backgroundColor = color6;
+  }
+
+  if (previousCards.length <= 1) {
+    if (!previousCard) {
+      previousCard = cardList[index];
+    }
+    previousCards.push(cardList[index].style.backgroundColor);
+  }
+  if (previousCards.length === 2) {
+    if (previousCards[0] === previousCards[1]) {
+      previousCard = null;
+      previousCards = [];
+      pairCount += 1;
+    } else if (previousCards[0] !== previousCards[1]) {
+      setTimeout(() => {
+        cardList[index].style.backgroundColor = colorBG;
+        previousCard.style.backgroundColor = colorBG;
+      }, 250);
+      setTimeout(() => {
+        previousCard = null;
+        previousCards = [];
+      }, 251);
+    }
+  }
+};
 
 const initializeGame = () => {
-    let countOfCards = [];
+  for (let i = 0; i < gameFieldCount; i += 1) {
+    gameField.innerHTML += '<div class="card"></div>';
+    cardList = document.querySelectorAll('.card');
+    cardList[i].classList.add(`card-${randomCardIDs[i]}`);
+    cardList[i].style.backgroundColor = colorBG;
+  }
+};
 
-    for (let i = 0; i < cardsList.length; i++) {
-        countOfCards.push(i)
+const removeOldCardsClass = () => {
+  for (let i = 0; i < gameFieldCount; i += 1) {
+    for (let j = 0; j < gameFieldCount; j += 1) {
+      cardList[i].classList.remove(`card-${[j]}`);
     }
+  }
+};
 
-    const cardNumberShuffle = _.shuffle(countOfCards);
-    
-    for (let i = 0; i < cardsList.length; i++) {
-        cardsList[i].style.display = 'block';
-        cardsList[i].style.backgroundColor = cardColors[cardColors.length - 1];
-        cardsList[i].classList.add(`card-${cardNumberShuffle[i]}`);
+const addNewCardsClass = () => {
+  randomCardIDs = _.shuffle(cardIDs);
+  for (let i = 0; i < gameFieldCount; i += 1) {
+    cardList[i].classList.add(`card-${randomCardIDs[i]}`);
+    cardList[i].style.backgroundColor = colorBG;
+  }
+};
+
+const checkIfWin = () => {
+  if (pairCount === gameFieldCount / 2) {
+    swal({
+      title: 'Good job!',
+      text: 'You collected all the cards! You can start over and improve!',
+      icon: 'success',
+    });
+    if (localStorage.winsTime) {
+      winTimes = +localStorage.winsTime;
+      winCounter.innerHTML = `Win count: ${localStorage.winsTime}`;
     }
-}
+    winTimes += 1;
+    localStorage.winsTime = winTimes;
+    winCounter.innerHTML = `Win count: ${winTimes}`;
+  }
+};
 
-const resetCards = () => {
-    for(let i = 0; i < cardsList.length; i++){
-        for(let j = 0;j < cardsList.length;j++){
-        cardsList[i].classList.remove(`card-${[j]}`);
-        }
-    }
-}
+const game = () => {
+  for (let i = 0; i < gameFieldCount; i += 1) {
+    // eslint-disable-next-line no-loop-func
+    cardList[i].addEventListener('click', () => {
+      handleCardClick(i);
+      moveCount += 1;
+      moveCounter.innerHTML = `Move count: ${moveCount}`;
+      checkIfWin();
+    });
+  }
+};
 
+const resetGameVariables = () => {
+  previousCard = null;
+  previousCards = [];
+  moveCount = 0;
+  randomCardIDs = [];
+  pairCount = 0;
+};
 
-// A function that takes the index of the card as an argument.It checks what class the card has and, when clicked, set background color from an array with background colors.
-// This is followed by a check if the array where there should be 2 background colors, whether it has a length of 1 or 0 (because it will be checked later on an array with a length of 1, 
-// and not with a length of 2), if 0, then it makes the next check if there is a value in which stores the previously selected card, if there is no card,
-// then a new value will be added to the variable and the background color of the card will be added to the array where there can be 2 cards for comparison
-// The final game check with cards that checks that the length of the array is 2 and if so, then the match of the background colors of 2 cards, if so then the card pair is added by 1 (for the next check) 
-// and the values in previousCard , previous are default
-// if the cards are not equal, then after 1 second the background for the cards returns to the standard one, and a little later the values in previousCard , previous are default.
-// Finally movecount +1
-const handleCardClick = (index: number) => {
-
-    const selected = cardsList[index].classList[1];
-
-    if (selected === 'card-0' || selected === 'card-1') {
-        cardsList[index].style.backgroundColor = cardColors[0];
-    } else if (selected === 'card-2' || selected === 'card-3') {
-        cardsList[index].style.backgroundColor = cardColors[1];
-    } else if (selected === 'card-4' || selected === 'card-5') {
-        cardsList[index].style.backgroundColor = cardColors[2];
-    }
-
-    if (previous.length <= 1) {
-        if (!previousCard) {
-            previousCard = cardsList[index]
-        }
-        previous.push(cardsList[index].style.backgroundColor)
-    }
-    if (previous.length === 2) {
-        if (previous[0] === previous[1]) {
-            previousCard = null
-            previous = []
-            cardPair++
-        } else if (previous[0] !== previous[1]) {
-            setTimeout(() => {
-                cardsList[index].style.backgroundColor = cardColors[cardColors.length - 1];
-                previousCard.style.backgroundColor = cardColors[cardColors.length - 1];
-            }, 500)
-            setTimeout(() => {
-                previousCard = null
-                previous = []
-            }, 600)
-        }
-
-    }
-    moveCount++;
-}
-
-const displayWinner = () => {
-    if (cardPair === cardsList.length / 2) {
-        setTimeout(() => {
-            window.alert('Win')
-        }, 200)
-    }
-}
-
-const resetGame = () => {
-    moveCount = 0;
-    previousCard = null; 
-    previous = [];
-    cardPair = 0;
-    for(let card of cardsList){
-        card.style.backgroundColor = cardColors[cardColors.length - 1]
-    };
-}
-
-//Events
-
-// When you press the start button, it checks if it was pressed, if not, the game starts
 btnStart.addEventListener('click', () => {
-    if (clickTime === 0) {
-        initializeGame();
-        clickTime++;
-    }
-})
+  btnStart.classList.add('btn__start-game--disabled');
+  btnReset.classList.remove('btn__reset-game--disabled');
+  moveCounter.classList.remove('move--disabled');
+  winCounter.classList.remove('win--disabled');
+  if (localStorage.winsTime) {
+    winCounter.innerHTML = `Win total: ${localStorage.winsTime}`;
+  }
+  if (!startBtnClick) {
+    initializeGame();
+    game();
+    startBtnClick = 1;
+  }
+});
 
-// Event listener for card by index
-for (let i = 0; i < cardsList.length; i++) {
-    cardsList[i].addEventListener('click', () => {
-        handleCardClick(i);
-        displayWinner()
-    })
-}
-
-btnRestart.addEventListener('click', () => {
-    resetCards();
-    resetGame();
-    initializeGame()
-})
-
-
-
-
+btnReset.addEventListener('click', () => {
+  removeOldCardsClass();
+  addNewCardsClass();
+  resetGameVariables();
+  moveCounter.innerHTML = `Move count: ${moveCount}`;
+});
