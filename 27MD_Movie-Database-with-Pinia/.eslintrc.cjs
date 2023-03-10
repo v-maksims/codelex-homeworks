1fbc2f7942832b908037ae21cdf7a21544dd62f5
Maksims Veiserts <maksims.veiserts@gmail.com>
2023-03-10 10:16:50 +0200

    27MD Ready for work

---
+++ 27MD_Movie-Database-with-Pinia/.eslintrc.cjs
@@ -0,0 +1,40 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+    root: true,
+    'extends': [
+        'plugin:vue/vue3-essential',
+        'eslint:recommended',
+        '@vue/eslint-config-typescript'
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest'
+    },
+    rules: {
+        'indent': [
+            'error',
+            4
+        ],
+        'space-before-function-paren': [
+            'error',
+            'always'
+        ],
+        'object-curly-spacing': [
+            'error',
+            'always'
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ]
+    }
+};
